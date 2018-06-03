09cef45ee4e6b0c8a0166d0776c61b5f579f76b7
zoltanpataki <zolee7777@gmail.com>
2018-06-03 19:28:04 +0200

    basic structure built

---
+++ src/main/java/com.codecool.sportSite/Model/Event.java
@@ -0,0 +1,59 @@
+package com.codecool.sportSite.Model;
+
+public class Event {
+    private String category;
+    private String event;
+    private String location;
+    private int volume;
+    private int place;
+    private String picture;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getPlace() {
+        return place;
+    }
+
+    public void setPlace(int place) {
+        this.place = place;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+}
